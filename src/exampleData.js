2bea403b33b4585727904b0f22c54467c25ed14a
Lxg1c <teran_2005@mail.ru>
2024-09-29 14:12:18 +0300

    first

---
+++ src/exampleData.js
@@ -0,0 +1,63 @@
+import { v4 as uuidv4 } from 'uuid'
+
+let exampleData = {
+	personalList: {
+		name: 'Truman Burbank',
+		email: 'trumanburbank@gmail.com',
+		phone: '+16507599755',
+		address: '226 E 122ND ST NEW YORK NY',
+	},
+
+	educationList: [
+		{
+			school: 'University of Maryland',
+			startDate: '08/2018',
+			endDate: '05/2022',
+			degree: 'Bachelors in Computer Science',
+			location: 'College Park, MD',
+			id: uuidv4(),
+		},
+		{
+			school: 'University of Maryland',
+			startDate: '08/2018',
+			endDate: '05/2022',
+			degree: 'Bachelors in Mathematics',
+			location: 'College Park, MD',
+			id: uuidv4(),
+		},
+	],
+	workList: [
+		{
+			company: 'Google',
+			position: 'Software Engineer',
+			startDate: '06/2022',
+			endDate: '01/2024',
+			location: 'Mountain View, CA',
+			description:
+				'Worked on the Google Assistant on Android Auto team, developing features for UI theming, Whatsapp Assistant CUJs, Music control, and more (Java, Kotlin, C++)',
+			id: uuidv4(),
+		},
+		{
+			company: 'JHU Applied Physics Lab',
+			position: 'Software Engineer Intern',
+			startDate: '06/2021',
+			endDate: '05/2022',
+			location: 'Laurel, MD',
+			description:
+				'Worked on internal tooling, developing a GPS GUI tool to parse and record satellite data. Tool was used in field tests and applauded by our customers (C++)',
+			id: uuidv4(),
+		},
+		{
+			company: 'JHU Applied Physics Lab',
+			position: 'Software Engineer Intern',
+			startDate: '05/2019',
+			endDate: '08/2019',
+			location: 'Laurel, MD',
+			description:
+				'Built internal tool to automate radar simulation runs, increasing team velocity and eliminating manual input errors for a team of 40+ users',
+			id: uuidv4(),
+		},
+	],
+}
+
+export default exampleData
